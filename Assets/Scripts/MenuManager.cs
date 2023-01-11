1e83abc9cf8d5253741271942b33fba911042dd0
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-11 15:02:02 +0100

    Add functionality to disappear the menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] InputActionReference _menuAction;
+    [SerializeField] GameObject _menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _menuAction.action.Enable();
+        _menuAction.action.performed += DisplayMenu;
+    }
+
+    private void DisplayMenu(InputAction.CallbackContext obj)
+    {
+        if (_menu.activeSelf)
+        {
+            _menu.SetActive(false);
+        }
+        else
+        {
+            _menu.SetActive(true);
+
+        }
+    }
+}
